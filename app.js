5bba75e2e3c2190afd1945c5c297fce04d4c6dcc
Hirzi Nurfakhrian <hirzinurfakhrian@gmail.com>
2023-07-16 22:10:31 +0700

    chore: add logger

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ import nodeCleanup from 'node-cleanup'
 import routes from './routes.js'
 import { init, cleanup } from './whatsapp.js'
 import cors from 'cors'
+import morgan from 'morgan'
+import chalk from 'chalk'
 
 const app = express()
 
@@ -11,6 +13,18 @@ const host = process.env.WA_SERVER_HOST || undefined
 const port = parseInt(process.env.WA_SERVER_PORT ?? 3300)
 
 app.use(cors())
+app.use(
+    morgan((tokens, req, res) => {
+        return [
+            chalk.cyan(tokens.date(req, res, 'iso')),
+            chalk.green.bold(tokens.method(req, res) + ':'),
+            chalk.gray(tokens.url(req, res)),
+            chalk.red.green(tokens.status(req, res)),
+            chalk.blue(tokens.res(req, res, 'content-length')),
+            chalk.yellow(tokens['response-time'](req, res) + ' ms'),
+        ].join(' ')
+    })
+)
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use('/', routes)
